99629262164ce978e968920b90db77eae7b036b6
kalash <kalash21504@gmail.com>
2025-10-06 19:51:17 +0530

    authenticion functionality done

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        
+        this.account = new Account(this.Client);
+    }
+
+    async createAccount({ email, username, password }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, username, password);
+
+            if (userAccount) {
+                return login({ email, username, password });
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login() {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getcurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
