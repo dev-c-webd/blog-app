cf00ff8611576215ff44809ca68018c2b538647c
kalash <kalash21504@gmail.com>
2025-10-08 19:14:48 +0530

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth.js"
+import {login, logout} from "./store/authSlice.js"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
   
-  return (
-    <div>App</div>
-  )
+  useEffect(() => {
+    authService.getcurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout);
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App;
